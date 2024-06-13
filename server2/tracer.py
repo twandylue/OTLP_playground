95af473398ea25856c5b4cc73005d61fda4a712b
AndyLu <laserlue@gmail.com>
2024-06-12 22:46:03 -0600

    refactor: refactor tracer.py
    Refactor tracer.py

--- server2/tracer.py
+++ server2/tracer.py
@@ -8,15 +8,17 @@ def init_TracerProvider(processor: SpanProcessor = None):
     Initialize TracerProvider
     """
 
-    resource: Resource = Resource(
-        attributes={
-            SERVICE_NAME: "andylu-http-example",
-        }
+    # NOTE: Initialize the TracerProvider
+    tracer_provider: TracerProvider = TracerProvider(
+        resource=Resource(
+            attributes={
+                SERVICE_NAME: "andylu-http-example",
+            }
+        )
     )
 
-    # NOTE: Initialize the TracerProvider
-    tracer_provider: TracerProvider = TracerProvider(resource=resource)
-    trace.set_tracer_provider(tracer_provider)
-
     # NOTE: Register the span processors with TracerProvider
-    tracer_provider.add_span_processor(processor)
+    if processor is not None:
+        tracer_provider.add_span_processor(processor)
+
+    trace.set_tracer_provider(tracer_provider)
